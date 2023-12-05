4ab1078e6d83a5fdf3b0a15345396ddc66a953d2
Filip Källström <filip.kallstrom@gmail.com>
2023-12-05 23:40:20 +0100

    Improve day 5

--- aoc-2023/src/day05/day05.kt
+++ aoc-2023/src/day05/day05.kt
@@ -2,49 +2,53 @@ package day05
 
 import util.*
 
-fun createMap(lines: List<String>): MutableList<Pair<LongRange, LongRange>> {
-    val ranges = mutableListOf<Pair<LongRange,LongRange>>()
+data class Mapping(val sourceRange: LongRange, val offset: Long)
+
+class Mapper(private val mappings: List<Mapping>) {
+
+    /** Remember last used mapping. */
+    private var mapping: Mapping? = null
+
+    fun mapValue(value: Long): Long {
+        if (mapping?.sourceRange?.contains(value) != true) {
+            mapping = mappings.firstOrNull { (source, _) -> source.contains(value) }
+        }
+        return value + (mapping?.offset ?: 0L)
+    }
+}
+
+fun createMapper(lines: List<String>): Mapper {
+    val ranges = mutableListOf<Mapping>()
     lines.forEach {
         val (destination, source, length) = it.split(" ").mapToLong()
-        ranges.add(source..<(source+length) to destination..<(destination+length))
+        val sourceRange = source..<source+length
+        val destinationOffset = destination-source
+        ranges.add(Mapping(sourceRange, destinationOffset))
     }
-    return ranges
+    return Mapper(ranges)
 }
 
 fun part1(input: String) {
     val blocks = readBlocks(input)
 
     val seeds = blocks.first().first().split(" ").drop(1).mapToLong()
-    val seedToSoil = createMap(blocks[1].drop(1))
-    val soilToFertilizer = createMap(blocks[2].drop(1))
-    val fertilizerTowater = createMap(blocks[3].drop(1))
-    val waterTolight = createMap(blocks[4].drop(1))
-    val lightTotemperature = createMap(blocks[5].drop(1))
-    val temperatureTohumidity = createMap(blocks[6].drop(1))
-    val humidityTolocation = createMap(blocks[7].drop(1))
+    val seedToSoil = createMapper(blocks[1].drop(1))
+    val soilToFertilizer = createMapper(blocks[2].drop(1))
+    val fertilizerToWater = createMapper(blocks[3].drop(1))
+    val waterToLight = createMapper(blocks[4].drop(1))
+    val lightToTemperature = createMapper(blocks[5].drop(1))
+    val temperatureToHumidity = createMapper(blocks[6].drop(1))
+    val humidityToLocation = createMapper(blocks[7].drop(1))
 
-    val minLocation = seeds.minOf {
-
-        var range = seedToSoil.firstOrNull { (source, dest) -> source.contains(it) }
-        var value = if (range == null) it else it - range.first.first + range.second.first
-
-        range = soilToFertilizer.firstOrNull { (source, dest) -> source.contains(value) }
-        value = if (range == null) value else value - range.first.first + range.second.first
-
-        range = fertilizerTowater.firstOrNull { (source, dest) -> source.contains(value) }
-        value = if (range == null) value else value - range.first.first + range.second.first
-
-        range = waterTolight.firstOrNull { (source, dest) -> source.contains(value) }
-        value = if (range == null) value else value - range.first.first + range.second.first
-
-        range = lightTotemperature.firstOrNull { (source, dest) -> source.contains(value) }
-        value = if (range == null) value else value - range.first.first + range.second.first
-
-        range = temperatureTohumidity.firstOrNull { (source, dest) -> source.contains(value) }
-        value = if (range == null) value else value - range.first.first + range.second.first
-
-        range = humidityTolocation.firstOrNull { (source, dest) -> source.contains(value) }
-        value = if (range == null) value else value - range.first.first + range.second.first
+    var value: Long
+    val minLocation = seeds.minOf { seed ->
+        value = seedToSoil.mapValue(seed)
+        value = soilToFertilizer.mapValue(value)
+        value = fertilizerToWater.mapValue(value)
+        value = waterToLight.mapValue(value)
+        value = lightToTemperature.mapValue(value)
+        value = temperatureToHumidity.mapValue(value)
+        value = humidityToLocation.mapValue(value)
 
         value
     }
@@ -56,36 +60,24 @@ fun part2(input: String) {
     val blocks = readBlocks(input)
 
     val seeds = blocks.first().first().split(" ").drop(1).mapToLong().chunked(2)
-    val seedToSoil = createMap(blocks[1].drop(1))
-    val soilToFertilizer = createMap(blocks[2].drop(1))
-    val fertilizerTowater = createMap(blocks[3].drop(1))
-    val waterTolight = createMap(blocks[4].drop(1))
-    val lightTotemperature = createMap(blocks[5].drop(1))
-    val temperatureTohumidity = createMap(blocks[6].drop(1))
-    val humidityTolocation = createMap(blocks[7].drop(1))
+    val seedToSoil = createMapper(blocks[1].drop(1))
+    val soilToFertilizer = createMapper(blocks[2].drop(1))
+    val fertilizerToWater = createMapper(blocks[3].drop(1))
+    val waterToLight = createMapper(blocks[4].drop(1))
+    val lightToTemperature = createMapper(blocks[5].drop(1))
+    val temperatureToHumidity = createMapper(blocks[6].drop(1))
+    val humidityToLocation = createMapper(blocks[7].drop(1))
 
+    var value: Long
     val minLocation = seeds.minOf {(start, amount) ->
-        (start..<start+amount).minOf {
-            var range = seedToSoil.firstOrNull { (source, _) -> source.contains(it) }
-            var value = if (range == null) it else it - range.first.first + range.second.first
-
-            range = soilToFertilizer.firstOrNull { (source, _) -> source.contains(value) }
-            value = if (range == null) value else value - range.first.first + range.second.first
-
-            range = fertilizerTowater.firstOrNull { (source, _) -> source.contains(value) }
-            value = if (range == null) value else value - range.first.first + range.second.first
-
-            range = waterTolight.firstOrNull { (source, _) -> source.contains(value) }
-            value = if (range == null) value else value - range.first.first + range.second.first
-
-            range = lightTotemperature.firstOrNull { (source, _) -> source.contains(value) }
-            value = if (range == null) value else value - range.first.first + range.second.first
-
-            range = temperatureTohumidity.firstOrNull { (source, _) -> source.contains(value) }
-            value = if (range == null) value else value - range.first.first + range.second.first
-
-            range = humidityTolocation.firstOrNull { (source, _) -> source.contains(value) }
-            value = if (range == null) value else value - range.first.first + range.second.first
+        (start..<start+amount).minOf { seed ->
+            value = seedToSoil.mapValue(seed)
+            value = soilToFertilizer.mapValue(value)
+            value = fertilizerToWater.mapValue(value)
+            value = waterToLight.mapValue(value)
+            value = lightToTemperature.mapValue(value)
+            value = temperatureToHumidity.mapValue(value)
+            value = humidityToLocation.mapValue(value)
 
             value
         }
