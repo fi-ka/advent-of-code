24dd5776bdd97bc097cb588bfdf489c1fca8396c
Filip Källström <filip.kallstrom@gmail.com>
2023-12-09 23:48:41 +0100

    Improve day 5 part 2

--- aoc-2023/src/day05/day05.kt
+++ aoc-2023/src/day05/day05.kt
@@ -1,10 +1,14 @@
 package day05
 
 import util.*
+import kotlin.math.max
+import kotlin.math.min
 
 data class Mapping(val sourceRange: LongRange, val offset: Long)
 
-class Mapper(private val mappings: List<Mapping>) {
+class Mapper(mappings: List<Mapping>) {
+
+    private val mappings = mappings.sortedBy { it.sourceRange.first }
 
     /** Remember last used mapping. */
     private var mapping: Mapping? = null
@@ -14,6 +18,35 @@ class Mapper(private val mappings: List<Mapping>) {
             mapping = mappings.firstOrNull { (source, _) -> source.contains(value) }
         }
         return value + (mapping?.offset ?: 0L)
+    }
+
+    fun mapRange(range: LongRange): List<LongRange> {
+        val mappedRanges = mutableListOf<LongRange>()
+        var ranges = listOf(range).sortedBy { it.first }
+
+        do {
+            var rangeIntersection = false
+            for (map in mappings) {
+                val nextRanges = mutableListOf<LongRange>()
+                ranges.forEach { r ->
+                    val beforeIntersection = r.first..min(r.last, map.sourceRange.first - 1)
+                    val intersection = max(r.first, map.sourceRange.first)..min(r.last, map.sourceRange.last)
+                    val afterIntersection = max(r.first, map.sourceRange.last + 1)..r.last
+
+                    if (!beforeIntersection.isEmpty())
+                        mappedRanges.add(beforeIntersection)
+                    if (!intersection.isEmpty()) {
+                        mappedRanges.add((intersection.first + map.offset)..(intersection.last + map.offset))
+                        rangeIntersection = true
+                    }
+                    if (!afterIntersection.isEmpty())
+                        nextRanges.add(afterIntersection)
+                }
+                ranges = nextRanges
+            }
+        } while (rangeIntersection && ranges.isNotEmpty())
+
+        return mappedRanges + ranges
     }
 }
 
@@ -59,31 +92,28 @@ fun part1(input: String) {
 fun part2(input: String) {
     val blocks = readBlocks(input)
 
-    val seeds = blocks.first().first().split(" ").drop(1).mapToLong().chunked(2)
-    val seedToSoil = createMapper(blocks[1].drop(1))
-    val soilToFertilizer = createMapper(blocks[2].drop(1))
-    val fertilizerToWater = createMapper(blocks[3].drop(1))
-    val waterToLight = createMapper(blocks[4].drop(1))
-    val lightToTemperature = createMapper(blocks[5].drop(1))
-    val temperatureToHumidity = createMapper(blocks[6].drop(1))
-    val humidityToLocation = createMapper(blocks[7].drop(1))
+    var seeds = blocks.first().first().split(" ").drop(1)
+        .mapToLong()
+        .chunked(2)
+        .map { (start, amount) -> start..<start+amount }
 
-    var value: Long
-    val minLocation = seeds.minOf {(start, amount) ->
-        (start..<start+amount).minOf { seed ->
-            value = seedToSoil.mapValue(seed)
-            value = soilToFertilizer.mapValue(value)
-            value = fertilizerToWater.mapValue(value)
-            value = waterToLight.mapValue(value)
-            value = lightToTemperature.mapValue(value)
-            value = temperatureToHumidity.mapValue(value)
-            value = humidityToLocation.mapValue(value)
+    val maps = listOf(
+        createMapper(blocks[1].drop(1)),
+        createMapper(blocks[2].drop(1)),
+        createMapper(blocks[3].drop(1)),
+        createMapper(blocks[4].drop(1)),
+        createMapper(blocks[5].drop(1)),
+        createMapper(blocks[6].drop(1)),
+        createMapper(blocks[7].drop(1)),
+    )
 
-            value
+    for (map in maps) {
+        seeds = seeds.flatMap { range ->
+            map.mapRange(range)
         }
     }
 
-    println(minLocation)
+    println(seeds.minOf { it.first })
 }
 
 fun main() {
