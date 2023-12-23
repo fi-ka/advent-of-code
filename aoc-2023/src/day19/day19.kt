8259cafb5198b351174b707300a40e5353533ca0
Filip Källström <filip.kallstrom@gmail.com>
2023-12-23 12:56:15 +0100

    WIP: day 19 part 2

--- aoc-2023/src/day19/day19.kt
+++ aoc-2023/src/day19/day19.kt
@@ -1,8 +1,8 @@
 package day19
 
 import util.*
-
-
+import kotlin.math.max
+import kotlin.math.min
 
 fun part1(input: String) {
     val blocks = readBlocks(input)
@@ -56,7 +56,107 @@ fun part1(input: String) {
 }
 
 fun part2(input: String) {
+    val blocks = readBlocks(input)
 
+    val workflowsLines = blocks[0]
+
+    val workflows = workflowsLines.associate { line ->
+        val (name, ruleLines, otherwise) = """(\w+)\{(.*),(\w+)\}""".toRegex().matchEntire(line)!!.destructured
+        val ruleMatches = """(\w)([<>])(\d+):(\w+)""".toRegex().findAll(ruleLines)
+            .map { it.groupValues }
+
+        val inverse = mutableMapOf<String, IntRange>()
+        val rules = mutableMapOf<String, MutableMap<String, IntRange>>()
+        for ((_, left, operator, value, result) in ruleMatches) {
+            if (result !in rules)
+                rules[result] = mutableMapOf()
+
+            val v = value.toInt()
+            if (operator == "<") {
+                rules[result]!![left] = 1..<v
+                inverse[left] = (v+1)..4000
+            } else {
+                rules[result]!![left] = (v+1)..4000
+                inverse[left] = 1..v
+            }
+        }
+        rules[otherwise] = inverse
+        name to rules
+    }
+
+    val partsInWorkflow = mutableMapOf(
+        "in" to mutableMapOf(
+            "x" to 1..4000,
+            "m" to 1..4000,
+            "a" to 1..4000,
+            "s" to 1..4000
+        )
+    )
+    val acceptedParts = mutableListOf<Map<String, IntRange>>()
+    while (partsInWorkflow.keys.any { it !in listOf("A", "R") }) {
+        println(partsInWorkflow)
+
+        partsInWorkflow.keys.filter { it !in listOf("A", "R") }.forEach { workflowName ->
+            workflows[workflowName]!!.forEach { (target, ranges) ->
+                if (target !in partsInWorkflow)
+                    partsInWorkflow[target] = partsInWorkflow[workflowName]!!.toMutableMap()
+
+
+                ranges.forEach { (key, value) ->
+                    if (key !in partsInWorkflow[target]!!) {
+                        partsInWorkflow[target]!![key] = value
+                    } else {
+                        val currRange = partsInWorkflow[target]!![key]!!
+                        val intersection: IntRange = max(currRange.first, value.first)..min(currRange.last, value.last)
+                        if (!intersection.isEmpty()) {
+                            partsInWorkflow[target]!![key] = intersection
+                        }
+                    }
+                }
+                if (target == "A") {
+                    acceptedParts.add(partsInWorkflow[target]!!)
+                    partsInWorkflow.remove("A")
+                }
+
+            }
+            partsInWorkflow.remove(workflowName)
+        }
+    }
+
+    println(partsInWorkflow)
+
+    val acceptedParts2 = mutableListOf<Map<String, IntRange>>()
+    acceptedParts.map {
+
+    }
+    println(acceptedParts)
+
+//    println(partsInWorkflow["A"]!!
+//        .values
+//        .fold(1L) { acc, r -> acc * (r.last-r.first+1) }
+//    )
+
+    // R={x=1..4000, m=2091..4000, a=1..2005, s=1..1350}
+    // A={x=1..4000, m=2091..4000, a=1..2005, s=1..1350}
+    //println(acceptedParts)
+//    parts.filter { part ->
+//        var current = "in"
+//
+//        do {
+//            val workflow = workflows[current]!!
+//            for (rule in workflow) {
+//                val next = rule(part)
+//                if (next != null) {
+//                    current = next
+//                    break
+//                }
+//            }
+//        } while(current !in listOf("A", "R"))
+//
+//        current == "A"
+//    }.sumOf {
+//        it.values.sum()
+//    }.let { println(it) }
 }
 
 fun main() {
@@ -67,12 +167,12 @@ fun main() {
     val test = """src/day${day.padStart(2, '0')}/test.txt"""
 
     runPart("Part 1") {
-        part1(test)
-         part1(input)
+//        part1(test)
+//         part1(input)
     }
 
     runPart("Part 2") {
-        // part2(test)
+         part2(test)
         // part2(input)
     }
 }
